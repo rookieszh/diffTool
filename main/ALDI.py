030957c4bca7a8218295ab5543744193e3feb949
Rookie Shen <rookie_szh@126.com>
2019-07-18 16:14:23 +0800

    For release 0.2.0

--- main/ALDI.py
+++ main/ALDI.py
@@ -1,4 +1,5 @@
 from OboardCamDisp import Ui_MainWindow
+from InfoWindow import Ui_MainWindow_2
 from sys import argv, exit
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtCore import QTimer, QEvent
@@ -14,16 +15,18 @@ class DiffCount(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.prepParameters()
         self.callBackFunctions()
-        self.Timer=QTimer()
-        self.OriginPathLE.installEventFilter(self)
-        self.NewPathLE.installEventFilter(self)
+        self.Timer = QTimer()
+        self.leftTE.installEventFilter(self)
+        self.rightTE.installEventFilter(self)
+        # self.OriginPathLE.installEventFilter(self)
+        # self.NewPathLE.installEventFilter(self)
 
     def prepParameters(self):
         self.OriginPath = None
         self.NewPath = None
-        self.ignore = None
-        self.force = None
-        self.txt = None
+        self.ignore = '--ignore-case'
+        self.force = '--force-parse-all'
+        self.txt = '> result.txt'
         self.cmd = 'diffcount'
         self.simple = '--for-program-reading'
         self.complex = '--print-files-info'
@@ -31,42 +34,51 @@ class DiffCount(QMainWindow, Ui_MainWindow):
 
     def callBackFunctions(self):
         self.GoBtn.clicked.connect(self.goComparison)
-        self.OriginPathBtn.clicked.connect(self.setOriginPath)
-        self.NewPathBtn.clicked.connect(self.setNewPath)
-        self.OneBtn.clicked.connect(self.setOneModel)
-        self.DiffBtn.clicked.connect(self.setComparisonModel)
-        self.SimpleBtn.clicked.connect(self.setSimpleModel)
-        self.ComplexBtn.clicked.connect(self.setComplexModel)
-        self.IgnoreCkb.stateChanged.connect(self.ignoreCase)
-        self.ForceCkb.stateChanged.connect(self.forceParseAll)
-        self.FileCkb.stateChanged.connect(self.exportToTxt)
-        self.OriginPathLE.textChanged.connect(self.originPathChange)
-        self.NewPathLE.textChanged.connect(self.newPathChange)
+        # self.OriginPathBtn.clicked.connect(self.setOriginPath)
+        # self.NewPathBtn.clicked.connect(self.setNewPath)
+        self.OneMB.changed.connect(self.setOneModel)
+        # self.OneBtn.clicked.connect(self.setOneModel)
+        self.DiffMB.changed.connect(self.setComparisonModel)
+        # self.DiffBtn.clicked.connect(self.setComparisonModel)
+        self.SimpleMB.changed.connect(self.setSimpleModel)
+        # self.SimpleBtn.clicked.connect(self.setSimpleModel)
+        self.ComplexMB.changed.connect(self.setComplexModel)
+        # self.ComplexBtn.clicked.connect(self.setComplexModel)
+        # self.IgnoreMB.changed.connect(self.ignoreCase)
+        # self.IgnoreCkb.stateChanged.connect(self.ignoreCase)
+        # self.ForceMB.changed.connect(self.forceParseAll)
+        # self.ForceCkb.stateChanged.connect(self.forceParseAll)
+        # self.FileMB.changed.connect(self.exportToTxt)
+        # self.FileCkb.stateChanged.connect(self.exportToTxt)
+        self.leftTE.textChanged.connect(self.leftPathChange)
+        self.rightTE.textChanged.connect(self.rightPathChange)
+        # self.OriginPathLE.textChanged.connect(self.originPathChange)
+        # self.NewPathLE.textChanged.connect(self.newPathChange)
         # self.IniBtn.clicked.connect(self.addToPath)
 
     def goComparison(self):
         self.Timer.start(1)
         timelb = clock()
-        if self.ChoiceSld.value() == 0:
+        if self.OneMB.isChecked():
             if self.OriginPath is not None:
                 self.cmd = self.cmd + ' ' + self.OriginPath + ' ' + '-c'
-                if self.ignore is not None:
+                if self.IgnoreMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.ignore
-                if self.force is not None:
+                if self.ForceMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.force
-                if self.ChoiceSld_2.value() == 0:
+                if self.SimpleMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.simple
                 else:
                     self.cmd = self.cmd + ' ' + self.complex
-                if self.txt is not None:
+                if self.FileMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.txt
                 info = Popen(self.cmd, shell=True, stdout=PIPE)
                 info = info.communicate()[0].decode()
-                self.InfoTE.setPlainText(info)
-                if self.ChoiceSld_2.value() == 0 and not self.FileCkb.isChecked():
+                self.newWindow(info)
+                if self.SimpleMB.isChecked() and not self.FileMB.isChecked():
                     print(info)
                     self.cmd = 'diffcount'
-                elif self.ChoiceSld_2.value() == 1 and not self.FileCkb.isChecked():
+                elif self.ComplexMB.isChecked() and not self.FileMB.isChecked():
                     lines = info.split('\r\n')
                     for line in lines:
                         line = line.strip()
@@ -94,23 +106,23 @@ class DiffCount(QMainWindow, Ui_MainWindow):
         else:
             if self.OriginPath is not None and self.NewPath is not None:
                 self.cmd = self.cmd + ' ' + self.OriginPath + ' ' + self.NewPath
-                if self.ignore is not None:
+                if self.IgnoreMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.ignore
-                if self.force is not None:
+                if self.ForceMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.force
-                if self.ChoiceSld_2.value() == 0:
+                if self.SimpleMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.simple
                 else:
                     self.cmd = self.cmd + ' ' + self.complex
-                if self.txt is not None:
+                if self.FileMB.isChecked():
                     self.cmd = self.cmd + ' ' + self.txt
                 info = Popen(self.cmd, shell=True, stdout=PIPE)
                 info = info.communicate()[0].decode()
-                self.InfoTE.setPlainText(info)
-                if self.ChoiceSld_2.value() == 0 and not self.FileCkb.isChecked():
+                self.newWindow(info)
+                if self.SimpleMB.isChecked() and not self.FileMB.isChecked():
                     print(info)
                     self.cmd = 'diffcount'
-                elif self.ChoiceSld_2.value() == 1 and not self.FileCkb.isChecked():
+                elif self.ComplexMB.isChecked() and not self.FileMB.isChecked():
                     lines = info.split('\r\n')
                     for line in lines:
                         line = line.strip()
@@ -145,51 +157,67 @@ class DiffCount(QMainWindow, Ui_MainWindow):
         self.TimeCostLCD.display(clock()-timelb)
         self.Timer.stop()
 
-    def setOriginPath(self):
-        dirname = QFileDialog.getExistingDirectory(self, "浏览", '.')
-        if dirname:
-            self.OriginPathLE.setText(dirname)
-            self.OriginPath = dirname
-
-    def setNewPath(self):
-        dirname = QFileDialog.getExistingDirectory(self, "浏览", '.')
-        if dirname:
-            self.NewPathLE.setText(dirname)
-            self.NewPath = dirname
+    # def setOriginPath(self):
+    #     dirname = QFileDialog.getExistingDirectory(self, "浏览", '.')
+    #     if dirname:
+    #         self.OriginPathLE.setText(dirname)
+    #         self.OriginPath = dirname
+    #
+    # def setNewPath(self):
+    #     dirname = QFileDialog.getExistingDirectory(self, "浏览", '.')
+    #     if dirname:
+    #         self.NewPathLE.setText(dirname)
+    #         self.NewPath = dirname
 
     def setOneModel(self):
-        self.ChoiceSld.setValue(0)
-        self.NewPathLE.setEnabled(False)
-        self.NewPathBtn.setEnabled(False)
+        if self.OneMB.isChecked() and self.DiffMB.isChecked():
+            self.DiffMB.setChecked(False)
+        elif not self.OneMB.isChecked() and not self.DiffMB.isChecked():
+            self.OneMB.setChecked(True)
+        # self.ChoiceSld.setValue(0)
+        # self.NewPathLE.setEnabled(False)
+        # self.NewPathBtn.setEnabled(False)
 
     def setComparisonModel(self):
-        self.ChoiceSld.setValue(1)
-        self.NewPathLE.setEnabled(True)
-        self.NewPathBtn.setEnabled(True)
+        if self.DiffMB.isChecked() and self.OneMB.isChecked():
+            self.OneMB.setChecked(False)
+        elif not self.DiffMB.isChecked() and not self.OneMB.isChecked():
+            self.DiffMB.setChecked(True)
+        # self.ChoiceSld.setValue(1)
+        # self.NewPathLE.setEnabled(True)
+        # self.NewPathBtn.setEnabled(True)
 
     def setSimpleModel(self):
-        self.ChoiceSld_2.setValue(0)
+        if self.SimpleMB.isChecked() and self.ComplexMB.isChecked():
+            self.ComplexMB.setChecked(False)
+        elif not self.SimpleMB.isChecked() and not self.ComplexMB.isChecked():
+            self.SimpleMB.setChecked(True)
+        # self.ChoiceSld_2.setValue(0)
 
     def setComplexModel(self):
-        self.ChoiceSld_2.setValue(1)
-
-    def ignoreCase(self):
-        if self.IgnoreCkb.isChecked():
-            self.ignore = '--ignore-case'
-        else:
-            self.ignore = None
-
-    def forceParseAll(self):
-        if self.ForceCkb.isChecked():
-            self.force = '--force-parse-all'
-        else:
-            self.force = None
-
-    def exportToTxt(self):
-        if self.FileCkb.isChecked():
-            self.txt = '> result.txt'
-        else:
-            self.txt = None
+        if self.ComplexMB.isChecked() and self.SimpleMB.isChecked():
+            self.SimpleMB.setChecked(False)
+        elif not self.ComplexMB.isChecked() and not self.SimpleMB.isChecked():
+            self.ComplexMB.setChecked(True)
+        # self.ChoiceSld_2.setValue(1)
+    #
+    # def ignoreCase(self):
+    #     if self.IgnoreCkb.isChecked():
+    #         self.ignore = '--ignore-case'
+    #     else:
+    #         self.ignore = None
+    #
+    # def forceParseAll(self):
+    #     if self.ForceCkb.isChecked():
+    #         self.force = '--force-parse-all'
+    #     else:
+    #         self.force = None
+    #
+    # def exportToTxt(self):
+    #     if self.FileCkb.isChecked():
+    #         self.txt = '> result.txt'
+    #     else:
+    #         self.txt = None
 
     # def dragEnterEvent(self, event):
     #     if event.mimeData().hasUrls():
@@ -198,20 +226,34 @@ class DiffCount(QMainWindow, Ui_MainWindow):
     #         event.ignore()
 
     def eventFilter(self, object, event):
-        if object is self.OriginPathLE:
+        # if object is self.OriginPathLE:
+        #     if event.type() == QEvent.DragEnter:
+        #         if event.mimeData().hasUrls():
+        #             # object.setText('')
+        #             event.accept()
+        #         else:
+        #             event.ignore()
+        if object is self.leftTE:
             if event.type() == QEvent.DragEnter:
                 if event.mimeData().hasUrls():
-                    # object.setText('')
+                    self.leftTE.setPlainText("")
                     event.accept()
                 else:
                     event.ignore()
-        if object is self.NewPathLE:
+        if object is self.rightTE:
             if event.type() == QEvent.DragEnter:
                 if event.mimeData().hasUrls():
-                    # object.setText('')
+                    self.rightTE.setPlainText("")
                     event.accept()
                 else:
                     event.ignore()
+        # if object is self.NewPathLE:
+        #     if event.type() == QEvent.DragEnter:
+        #         if event.mimeData().hasUrls():
+        #             # object.setText('')
+        #             event.accept()
+        #         else:
+        #             event.ignore()
             # if event.type() == QEvent.Drop:
             #     if event.mimeData().hasUrls():  # if file or link is dropped
             #         urlcount = len(event.mimeData().urls())  # count number of drops
@@ -224,18 +266,40 @@ class DiffCount(QMainWindow, Ui_MainWindow):
             return False
         return False
 
-    def originPathChange(self):
-        if 0 == self.OriginPathLE.text().find('file:///'):
-            self.OriginPathLE.setText(self.OriginPathLE.text().replace('file:///', ''))
-            self.OriginPath = self.OriginPathLE.text()
+    # def originPathChange(self):
+    #     if 0 == self.OriginPathLE.text().find('file:///'):
+    #         self.OriginPathLE.setText(self.OriginPathLE.text().replace('file:///', ''))
+    #         self.OriginPath = self.OriginPathLE.text()
 
-    def newPathChange(self):
-        if 0 == self.NewPathLE.text().find('file:///'):
-            self.NewPathLE.setText(self.NewPathLE.text().replace('file:///', ''))
-            self.NewPath = self.NewPathLE.text()
+    def leftPathChange(self):
+        if 0 == self.leftTE.toPlainText().find('file:///'):
+            self.leftTE.setPlainText(self.leftTE.toPlainText().replace('file:///', ''))
+            self.OriginPath = self.leftTE.toPlainText()
+
+    def rightPathChange(self):
+        if 0 == self.rightTE.toPlainText().find('file:///'):
+            self.rightTE.setPlainText(self.rightTE.toPlainText().replace('file:///', ''))
+            self.NewPath = self.rightTE.toPlainText()
+
+    # def newPathChange(self):
+    #     if 0 == self.NewPathLE.text().find('file:///'):
+    #         self.NewPathLE.setText(self.NewPathLE.text().replace('file:///', ''))
+    #         self.NewPath = self.NewPathLE.text()
 
     # def addToPath(self):
-    #     os.environ["PATH"] = os.environ["PATH"] + ';' + str(sys.path[0])+';'
+    #     # os.environ["PATH"] = os.environ["PATH"] + ';' + str(sys.path[0])+';'
+    #     self.InfoTE.setPlainText("yes")
+
+    def newWindow(self, information):
+        self.ui = ChildInfo()
+        self.ui.traninfo(info = information)
+        self.ui.show()
+
+
+class ChildInfo(QMainWindow, Ui_MainWindow_2):
+    def __init__(self, parent=None):
+        super(ChildInfo, self).__init__(parent)
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
---
+++ main/InfoWindow.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'InfoWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow_2(object):
+    def traninfo(self, info):
+        self.textEdit.setPlainText(info)
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(950, 262)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(0, 0, 950, 240))
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Result"))
+
